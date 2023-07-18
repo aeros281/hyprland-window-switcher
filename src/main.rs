924c9d0c2186cd58a97de1dca2deb6ca94b54dcc
Anh Le Tuan Tran <tranletuananh1992@gmail.com>
2023-07-18 23:51:35 +0700

    create subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use clap::Parser;
 use hyprland::data::{Client, Clients, WorkspaceBasic};
-use hyprland::dispatch::{Dispatch, DispatchType, WindowIdentifier};
+use hyprland::dispatch::{
+    Dispatch, DispatchType, WindowIdentifier, WorkspaceIdentifierWithSpecial,
+};
 use hyprland::prelude::*;
 use rofi;
 use std::collections::HashMap;
+
+mod cli;
 
 fn get_titles(clients: &Vec<Client>) -> HashMap<&String, i32> {
     clients
@@ -18,7 +23,7 @@ fn get_titles(clients: &Vec<Client>) -> HashMap<&String, i32> {
         .collect()
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn switch_window_by_titles() -> Result<(), Box<dyn std::error::Error>> {
     let clients = Clients::get()?.to_vec();
     println!("{clients:?}");
     let titles = get_titles(&clients);
@@ -32,4 +37,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Dispatch::call(DispatchType::FocusWindow(WindowIdentifier::ProcessId(pid)))?;
 
     Ok(())
+}
+
+fn switch_workspaces() -> Result<(), Box<dyn std::error::Error>> {
+    let workspaces: Vec<String> = (1..11).into_iter().map(|val| val.to_string()).collect();
+
+    let choice = rofi::Rofi::new(&workspaces).run()?;
+    println!("Choice: {}", choice);
+    let workspace_id: i32 = choice.parse::<i32>().unwrap();
+
+    Dispatch::call(DispatchType::Workspace(WorkspaceIdentifierWithSpecial::Id(
+        workspace_id,
+    )))?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    match cli::Cli::parse() {
+        cli::Cli::SwitchWindowByTitle => switch_window_by_titles(),
+        cli::Cli::SwitchWorkspace => switch_workspaces(),
+    }
 }
