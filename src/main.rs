dfbe1453e103d52ce1c715f163ed38eea966c244
Anh Le Tuan Tran <tranletuananh1992@gmail.com>
2023-07-20 12:39:49 +0700

    refactor main functions into module

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,17 @@
 use clap::Parser;
-use hyprland::data::{Client, Clients, Monitors, WorkspaceBasic};
-use hyprland::dispatch::{
-    Dispatch, DispatchType, MonitorIdentifier, WindowIdentifier, WorkspaceIdentifierWithSpecial,
-};
-use hyprland::prelude::*;
-use itertools::Itertools;
-use rofi;
-use std::collections::HashMap;
+use cli::{Cli, MonitorIndex};
 
 mod cli;
-
-fn get_titles(clients: &Clients) -> HashMap<&String, &hyprland::shared::Address> {
-    clients
-        .iter()
-        .filter(|Client { title, .. }| !title.is_empty())
-        .filter(
-            |Client {
-                 workspace: WorkspaceBasic { id, .. },
-                 ..
-             }| *id != -99, // id -99 is special workspace
-        )
-        .map(|Client { title, address, .. }| (title, address))
-        .collect()
-}
-
-fn switch_window_by_titles() -> Result<(), Box<dyn std::error::Error>> {
-    let clients = Clients::get()?;
-    let titles = get_titles(&clients);
-    let entries = Vec::from_iter(titles.keys());
-
-    let choice = rofi::Rofi::new(&entries).run()?;
-    println!("Choice: {}", choice);
-    let address = titles[&choice].clone();
-
-    println!("Address: {:?}", address);
-    Dispatch::call(DispatchType::FocusWindow(WindowIdentifier::Address(
-        address,
-    )))?;
-
-    Ok(())
-}
-
-fn dispatch_on_monitor_with_relative_index<F>(
-    index: usize,
-    cb: F,
-) -> Result<(), Box<dyn std::error::Error>>
-where
-    F: Fn(&String) -> Result<(), Box<dyn std::error::Error>>,
-{
-    let mut monitors = Monitors::get()?.to_vec();
-    monitors.sort_by_key(|m| m.x);
-    let monitor_name = &monitors.get(index).unwrap().name;
-    cb(monitor_name)
-}
-
-fn switch_monitor_by_index(index: usize) -> Result<(), Box<dyn std::error::Error>> {
-    dispatch_on_monitor_with_relative_index(index, |monitor_name| {
-        Dispatch::call(DispatchType::MoveCurrentWorkspaceToMonitor(
-            MonitorIdentifier::Name(&monitor_name),
-        ))?;
-        Ok(())
-    })
-}
-
-fn focus_monitor_by_index(index: usize) -> Result<(), Box<dyn std::error::Error>> {
-    dispatch_on_monitor_with_relative_index(index, |monitor_name| {
-        Dispatch::call(DispatchType::FocusMonitor(MonitorIdentifier::Name(
-            &monitor_name,
-        )))?;
-        Ok(())
-    })
-}
-
-fn switch_workspaces() -> Result<(), Box<dyn std::error::Error>> {
-    let mut workspace_hash = HashMap::new();
-    workspace_hash.insert("01. Terminal".to_string(), 1);
-    workspace_hash.insert("02. Browser (Firefox)".to_string(), 2);
-    workspace_hash.insert("03. File manager".to_string(), 3);
-    workspace_hash.insert("04. Media player".to_string(), 4);
-    workspace_hash.insert("05. Git".to_string(), 5);
-    workspace_hash.insert("06. none".to_string(), 6);
-    workspace_hash.insert("07. none".to_string(), 7);
-    workspace_hash.insert("08. none".to_string(), 8);
-    workspace_hash.insert("09. Email".to_string(), 9);
-    workspace_hash.insert("10. Communication (Chat, Slack)".to_string(), 10);
-    let entries = Vec::from_iter(workspace_hash.keys().sorted());
-
-    let choice = rofi::Rofi::new(&entries).run()?;
-    println!("Choice: {}", choice);
-    let workspace_id: i32 = workspace_hash[&choice];
-
-    Dispatch::call(DispatchType::Workspace(WorkspaceIdentifierWithSpecial::Id(
-        workspace_id,
-    )))?;
-    Ok(())
-}
-
-fn cycle_window_with_same_class() -> Result<(), Box<dyn std::error::Error>> {
-    let Client {
-        class: active_class,
-        address: active_address,
-        ..
-    } = Client::get_active()?.unwrap();
-    let clients = Clients::get()?
-        .filter(|Client { class, .. }| *class == active_class)
-        .collect::<Vec<Client>>();
-
-    let mut next_index: usize = 0;
-    for (index, c) in clients.iter().enumerate() {
-        if format!("{:?}", c.address) == format!("{:?}", active_address) {
-            next_index = index + 1;
-        }
-    }
-
-    next_index = next_index % clients.len();
-    let Client {
-        address: next_address,
-        ..
-    } = clients.get(next_index).unwrap();
-
-    Dispatch::call(DispatchType::FocusWindow(WindowIdentifier::Address(
-        next_address.clone(),
-    )))?;
-    Ok(())
-}
+mod dispatcher;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     match cli::Cli::parse() {
-        cli::Cli::SwitchWindowByTitle => switch_window_by_titles(),
-        cli::Cli::SwitchWorkspace => switch_workspaces(),
-        cli::Cli::CycleWindowWithSameClass => cycle_window_with_same_class(),
-        cli::Cli::MoveWorkspaceToMonitor(cli::MonitorIndex { index }) => {
-            switch_monitor_by_index(index)
+        Cli::SwitchWindowByTitle => dispatcher::switch_window_by_titles(),
+        Cli::SwitchWorkspace => dispatcher::switch_workspaces(),
+        Cli::CycleWindowWithSameClass => dispatcher::cycle_window_with_same_class(),
+        Cli::MoveWorkspaceToMonitor(MonitorIndex { index }) => {
+            dispatcher::switch_monitor_by_index(index)
         }
-        cli::Cli::FocusMonitor(cli::MonitorIndex { index }) => focus_monitor_by_index(index),
+        Cli::FocusMonitor(MonitorIndex { index }) => dispatcher::focus_monitor_by_index(index),
     }
 }
